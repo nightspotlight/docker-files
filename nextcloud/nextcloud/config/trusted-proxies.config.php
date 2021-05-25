a8228230e384171c25b2ac4086fa897e7849cc42
Roman Eremeev <nightspotlight@users.noreply.github.com>
2021-05-25 23:43:23 +0200

    Add realip config

---
+++ nextcloud/nextcloud/config/trusted-proxies.config.php
@@ -0,0 +1,12 @@
+<?php
+$CONFIG = array (
+  'trusted_proxies' =>
+  array (
+    0 => '172.16.0.0/12',
+  ),
+  'forwarded_for_headers' =>
+  array (
+    0 => 'HTTP_X_FORWARDED_FOR',
+    1 => 'HTTP_X_REAL_IP',
+  ),
+);
